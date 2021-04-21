e7af1db709b299f2bdc4f1c17a4000e03f149af5
FS4GRINGO <82925244+FS4GRINGO@users.noreply.github.com>
2021-04-21 02:23:25 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ apt-get install libwebp
 apt-get install ffmpeg
 apt-get install wget
 apt-get install tesseract
-wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
+wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/FS4GRINGO/BOT-GRINGO"
 npm audit fix
 npm i imgbb-uploader
 npm cache clean -f
